cc5be26f88933011fcdd207c00623c26007729ee
Wick007 <a.wick17@gmail.com>
2017-03-28 14:26:45 -0400

    asdasd

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-
-
-
   function getAPI(){
     const url= "http://localhost:3000/flutrack?s=feverANDcoughORfever"
     $.getJSON(url, function(data){
-      console.log(data)
+      var results = data.map(function(d){
+        `<p> ${d.aggravate} </p>
+        <p> ${d.longitude} </p>
+        <p> ${d.latitude} </p>
+        <p> ${d.tweet_date} </p>
+        <p> ${d.user_name} </p>
+        <p> ${d.tweet_text} </p>`
+
+      })
+      console.log(results)
+      $("#displayData").html(results)
     })
   }
