d81802235f79d9c9f3cd56ab3442c3e6b014df82
nanyr <nanyramirez0@gmail.com>
2017-03-28 16:45:04 -0400

    added geolocation, increased search results

--- fevertweeter/app/controllers/flutrack_controller.rb
+++ fevertweeter/app/controllers/flutrack_controller.rb
@@ -2,6 +2,6 @@ class FlutrackController < ApplicationController
   require 'rest_client'
 
   def index
-    render json: RestClient.get("http://api.flutrack.org/?s=#{params[:s]}")
+    render json: RestClient.get("http://api.flutrack.org/?time=7")
   end
 end
