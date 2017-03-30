5a6dad881837cec89b415c91ac12b4dcb32108d3
Wick007 <a.wick17@gmail.com>
2017-03-30 13:56:58 -0400

    mvc

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
 $(()=>{
   //get api
 
+// initiate map
+  const map = initMap();
+
   FluTracker.all.then((fluData)=>{
-    let $map=$('#map')
-    let fluController= new FluController(fluData, $map)
+    let $map = map
+    let fluController = new FluController(fluData, $map)
   })
 })
-
-
-function heatMap(){
-  const url= "http://localhost:3000/flutrack"
-  var locations=[]
-
-  $.getJSON(url).then(function(data){
-    locations= data.map((d)=>{
-      return [d.latitude, d.longitude, d.tweet_text]
-      })
-    return locations
-  }).then(function(locations){
-    heatmap = new google.maps.visualization.HeatmapLayer({
-      data: getPoints(locations),
-      radius: 35,
-      map: map
-    });
-  })
-}
---
+++ js/views/fluView.js
@@ -0,0 +1,27 @@
+class FluView {
+  static renderMap($map, locations, points) {
+    let heatmap = new google.maps.visualization.HeatmapLayer({
+      data: points,
+      radius: 35,
+      map: $map
+
+    })
+    var infowindow = new google.maps.InfoWindow({});
+
+    locations.forEach(function(location) {
+        let marker = new google.maps.Marker({
+            position: new google.maps.LatLng(location[0], location[1]),
+            icon: "https://upload.wikimedia.org/wikipedia/commons/f/f1/Ski_trail_rating_symbol_red_circle.png",
+            title: location[2],
+            map: map
+        });
+        google.maps.event.addListener(marker,'mouseover', function() {
+            infowindow.setContent(marker["title"])
+            infowindow.open(map, this);
+        });
+
+        google.maps.event.addListener(marker,'mouseout', function() {
+            infowindow.close();
+        });
+  }
+}
